6706df5961c157fc70a9bc88c20f2d35c2be0d7d
Ny <thoeunthonny4@gmail.com>
2026-01-06 11:53:01 +0700

    all

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-
-import Experience from './pages/Experience';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
@@ -18,8 +16,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home darkMode={darkMode} setDarkMode={setDarkMode} />} />
           <Route path="/projects" element={<Projects darkMode={darkMode} setDarkMode={setDarkMode} />} />
-
-          <Route path="/experience" element={<Experience darkMode={darkMode} setDarkMode={setDarkMode} />} />
           <Route path="/contact" element={<Contact darkMode={darkMode} setDarkMode={setDarkMode} />} />
           <Route path="/about" element={<About darkMode={darkMode} setDarkMode={setDarkMode} />} />
           <Route path="*" element={<NotFound darkMode={darkMode} setDarkMode={setDarkMode} />} />
